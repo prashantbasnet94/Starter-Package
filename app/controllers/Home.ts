00466e500c1e2ce32267cc2fa6c4715e1c3f8204
Prashant Basnet <30943807+prashantbasnet94@users.noreply.github.com>
2020-02-13 12:13:12 -0600

    high and close price for candle

--- app/controllers/Home.ts
+++ app/controllers/Home.ts
@@ -11,8 +11,41 @@ export class Home {
 
     async test(req:any,res:any){
         let ticker = await binance.prices();
-        console.log(`Price of BNB: ${ticker.BNBUSDT}`);
-        res.send(ticker.BNBUSDT);
+
+        //console.log(ticker)
+        binance.prices('', (error:any, ticker:any) => {
+           // console.log("Price of BNB: ", ticker);
+        });
+
+
+        binance.prevDay(false, (error:any, prevDay:any) => {
+            // console.log(prevDay); // view all data
+            for ( let obj of prevDay ) {
+                let symbol = obj.symbol;
+                if(obj.priceChangePercent<-20 ){
+                  //  console.log(symbol+" volume:"+obj.volume+" change: "+obj.priceChangePercent+"%");
+                }
+
+            }
+        });
+
+        // Intervals: 1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
+        binance.candlesticks("BNBBTC", "5m", (error:any, ticks:any, symbol:any) => {
+            console.log("candlesticks()", ticks);
+            let last_tick = ticks[ticks.length - 1];
+            let [time, open, high, low, close, volume, closeTime, assetVolume, trades, buyBaseVolume, buyAssetVolume, ignored] = last_tick;
+            console.log(symbol+" last close: "+close);
+            console.log(symbol+" high: "+high);
+
+        }, {limit: 500, endTime: 1514764800000});
+
+
+        binance.trades("ETHBTC", (error:any, trades:any, symbol:any) => {
+           // console.log(symbol+" trade history", trades);
+            res.send(symbol+"  trade history   "+trades);
+
+        });
+
     }
 
 

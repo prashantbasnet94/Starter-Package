1a00044f014a85bccc35cc148d25a7806638f267
Prashant Basnet <30943807+prashantbasnet94@users.noreply.github.com>
2020-02-13 11:37:27 -0600

    bnb usd price

--- app/controllers/Home.ts
+++ app/controllers/Home.ts
@@ -1,11 +1,18 @@
 import {Response} from "express";
+const Binance = require('node-binance-api');
 
+const binance = new Binance().options({
+    APIKEY: 'ABBXWiCIBjYtiybb7RHXv1KM52zc5c6ZwVWrYO3FoUCnWH40jloIW7csVCywP8Q8',
+    APISECRET: 'DSzbo5YNTsViNNQ84sdwNiLEMkn1WVqEz8cW3DUICMpVBKLin40xctCi4OzhwwSh'
+});
 
 export class Home {
 
 
-    test(req:any,res:any){
-         res.send('test');
+    async test(req:any,res:any){
+        let ticker = await binance.prices();
+        console.log(`Price of BNB: ${ticker.BNBUSDT}`);
+        res.send(ticker.BNBUSDT);
     }
 
 
